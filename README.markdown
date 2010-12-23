79c5a37b2ca3f179ac62a7c5982b201346504617
daniel joo <askjoo@gmail.com>
2010-12-23 16:00:12 -0500

    initial commit

---
+++ README.markdown
@@ -0,0 +1,9 @@
+# HAERFEST.US: cultivating modern aesthetics to take on forms familiar yet unexpected
+
+soft site web launch for [haerfest.us](http://haerfest.us) built using [*Ruby on Rails*]
+
+
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HaerfestUs::Application.config.secret_token = '031308683d1601757f277fef0c8221678a7dd5265ef36e6a28a35c6869f2a156b47ac013dc23ae3e42bb8524dda4a04ce399426c1ca719a5fe3965f97a661feb'
