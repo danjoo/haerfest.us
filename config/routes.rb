409f239601052df41c2c0b13f122ed91b1f900f9
daniel joo <askjoo@gmail.com>
2010-12-23 16:28:01 -0500

    changed home directory

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ HaerfestUs::Application.routes.draw do
   get "pages/about"
 
   get "pages/contact"
+  
+  root :to => "pages#home"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
