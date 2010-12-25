040936e6ff61662c04ab62701aa75f217377a2e3
Daniel Joo <dan@haerfest.us>
2010-12-25 22:31:44 +0000

    deleted old model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(:version => 20101225215812) do
 
   create_table "images", :force => true do |t|
@@ -20,8 +19,5 @@ ActiveRecord::Schema.define(:version => 20101225215812) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-=======
-ActiveRecord::Schema.define(:version => 0) do
->>>>>>> 830e18591ac9af54558abed6442cb2c24d90dced
 
 end
