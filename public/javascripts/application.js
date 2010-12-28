d7fe7e4f251655bf6dfdbb02ff66f95fbe2b6511
Daniel Joo <dan@haerfest.us>
2010-12-28 18:20:57 +0000

    jquery changes and nav change

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+/*
+ $(document).ready(function(){
+   $("a").click(function(event){
+     alert("Thanks for visiting!");
+   });
+ });
+*/
+
 /* no longer needed now that we have partials
 $(document).ready(function() {
   $('div.pagination a').live('click', function() {
