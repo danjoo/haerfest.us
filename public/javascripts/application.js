6f189359f744a592ae61b01da847c543d54ab626
daniel joo <askjoo@gmail.com>
2010-12-27 22:00:41 -0500

    testing slide show

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+$(document).ready(function() {
+  $('div.pagination a').live('click', function() {
+    $('#slideshow').fadeToggle("fast", "swing").load(this.href, addClickHandlers)
+    return false
+  })
+})
+
