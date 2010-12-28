4d80e498f9c54fb2afea9468efc99409648a8840
daniel joo <askjoo@gmail.com>
2010-12-28 12:13:23 -0500

    added better fade

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,29 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+/* no longer needed now that we have partials
 $(document).ready(function() {
   $('div.pagination a').live('click', function() {
-    $('#slideshow').fadeOut("slow", function() {
-      // Animation complete.
-      $('#slideshow').load(this.href);
-    })
+    $('#slideshow').hide().load($(this).attr("href"), function(){
+        $(this).fadeIn("slow");
+    });
   })
+})
+*/
+
+$(function() {
+    var $active = $('#slideshow IMG.active');
+    
+    // use this to pull the images in the order they appear in the markup
+    var $next =  $active.next().length ? $active.next()
+        : $('#slideshow IMG:first');
+
+    $active.addClass('last-active');
+
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
 })
 
