15125c697ebd4bf12ce3153b16143683b12f7aaf
daniel joo <askjoo@gmail.com>
2011-01-16 03:40:06 -0500

    .. burnt

--- public/javascripts/jquery.lightbox-0.5.js
+++ public/javascripts/jquery.lightbox-0.5.js
@@ -28,8 +28,8 @@
 			fixedNavigation:		false,		// (boolean) Boolean that informs if the navigation (next and prev button) will be fixed or not in the interface.
 			// Configuration related to images
 			imageLoading:			'images/lightbox/lightbox-ico-loading.gif',		// (string) Path and the name of the loading icon
-			imageBtnPrev:			'images/lightbox/lightbox-btn-prev.gif',			// (string) Path and the name of the prev button image
-			imageBtnNext:			'images/lightbox/lightbox-btn-next.gif',			// (string) Path and the name of the next button image
+			imageBtnPrev:			'images/lightbox/arrow_left.png',			// (string) Path and the name of the prev button image
+			imageBtnNext:			'images/lightbox/arrow_right.png',			// (string) Path and the name of the next button image
 			imageBtnClose:			'images/lightbox/lightbox-btn-close.gif',		// (string) Path and the name of the close btn
 			imageBlank:				'images/lightbox/lightbox-blank.gif',			// (string) Path and the name of a blank image (one pixel)
 			// Configuration related to container image box
